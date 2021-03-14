9bdc70c74b34687e185c257a0afe5ec3c7ec6a61
Nicolas Mendonca <ae_nmendonca@idcontractors.net>
2021-03-14 19:17:04 -0300

    Adds google authentication

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	env: {
+		API_KEY: 'AIzaSyAAHzBYK5qKfzwdjiRhfySQU3bBKmo_gF0',
+		AUTH_DOMAIN: 'stock-manager-b8e81.firebaseapp.com',
+		PROJECT_ID: 'stock-manager-b8e81',
+		STORAGE_BUCKET: 'stock-manager-b8e81.appspot.com',
+		MESSAGING_SENDER_ID: '802025046629',
+		APP_ID: '1:802025046629:web:841ada19c7a3d18fc87c8c',
+		MEASUREMENT_ID: 'G-1FW3MNJPND',
+	},
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,36 @@
+import React from 'react';
 import Head from 'next/head';
+import { Flex } from '@chakra-ui/layout';
+import { MainButton } from '../components/Button';
+import { firebase, firebaseAuth } from '../firebase';
 
 export default function Home() {
+	const [isLoginIn, setIsLoginIn] = React.useState(false);
+	function login() {
+		setIsLoginIn(true);
+		const provider = new firebase.auth.GoogleAuthProvider();
+		firebaseAuth
+			.signInWithPopup(provider)
+			.then(() => setIsLoginIn(false))
+			.catch(() => setIsLoginIn(false));
+	}
+
 	return (
-		<div>
+		<>
 			<Head>
 				<title>Create Next App</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-
-			<main>Hello</main>
-		</div>
+			<Flex
+				h="full"
+				alignItems="center"
+				justifyContent="center"
+				style={{ height: '100vh' }}
+			>
+				<MainButton onClick={login} disabled={isLoginIn}>
+					Login
+				</MainButton>
+			</Flex>
+		</>
 	);
 }
